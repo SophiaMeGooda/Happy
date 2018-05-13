d4aaec1cbaa9814c5712d2fee292c472fd7b6709
Sophia Wang <sophia.wang@ittmom.com>
2018-05-13 15:48:31 +0800

    2nd

--- mesupergood.py
+++ mesupergood.py
@@ -2,5 +2,5 @@ days_in_week = 7
 hours_in_day = 24
 minutes_in_hour = 60
 minutes_in_week = days_in_week * hours_in_day * minutes_in_hour
-print("hello, the minutes in a week are shown below.")
+print("hello, the minutes in a week are shown below.12")
 print(minutes_in_week)
