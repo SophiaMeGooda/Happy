953e12542e1626f36aea7f5e80a1c2ca41c1aa0b
Sophia Wang <sophia.wang@ittmom.com>
2018-05-15 17:58:56 +0800

    modify MathTools

--- MathTools.py
+++ MathTools.py
@@ -6,6 +6,7 @@ while True:
     print('5. triangle')
     print('quit')
     users_options = input('please press one of the following choices:  ')
+
     if users_options == '1':
         print('1')
         print('please tell me how wide the room is:')
@@ -18,6 +19,7 @@ while True:
         print(area)
         print("the perimeter is:")
         print(perimeter)
+
     elif users_options == '2':
         print('2')
         radius = input('please enter the radius of your circle:  ')
@@ -32,32 +34,37 @@ while True:
         perimeter = diameter*pi
         print('the perimeter of your circle is:')
         print(perimeter)
+
     elif users_options == '3':
         upperline = input('the upper or topline of the trapezoid is:  ')
-        upperline = int(upperline)
+        upperline = float(upperline)
         bottomline = input('the bottomline of the trapezoid is:  ')
-        bottomline = int(bottomline)
+        bottomline = float(bottomline)
         height = input('the height of the trapezoid is:  ')
-        height = int(height)
+        height = float(height)
         area = (upperline+bottomline)*height/2
         print('the area of the trapezoid is:')
         print(area)
     elif users_options == '4':
+
         height = input('the height of the parallelogram is:  ')
-        height = int(height)
+        height = float(height)
         base = input('the base of the parallelogram is:  ')
-        base = int(base)
+        base = float(base)
         area = height*base
         print(area)
     elif users_options == '5':
+
         height = input('the height of the triangle is:  ')
-        height = int(height)
+        height = float(height)
         base = input('the base of the triangle is:  ')
-        base = int(base)
+        base = float(base)
         area = height*base/2
         print(area)
+
     elif users_options == 'quit':
-        print('bye, have a nice day.')
+        print('Bye, have a nice day. Thank you for veiwing')
         break
+
     else:
         print('please choose the correct options')
